2a8fd40346314f6bedc599827ad70b6cb9ffd1b9
Vincent <vincentnewkirk@gmail.com>
2016-12-07 00:00:55 -1000

    basic visibility filter tests written and implemented

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as actions from './actions/actions';
+import { VisibilityFilters } from './actions/actions';
+import reducer from './reducers/reducer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -9,7 +10,22 @@ it('renders without crashing', () => {
 });
 
 describe('ACTIONS', () => {
-  it('should be defined and exist', () => {
-    expect(actions).toBeDefined();
+  it('should be exported correctly', () => {
+    expect(VisibilityFilters).toBeDefined();
+  });
+  it('should have a SHOW_ALL action', () => {
+    expect(VisibilityFilters.SHOW_ALL).toBeDefined();
+  });
+  it('should have a SHOW_COMPLETED action', () => {
+    expect(VisibilityFilters.SHOW_COMPLETED).toBeDefined();
+  });
+  it('should have a SHOW_ACTIVE action', () => {
+    expect(VisibilityFilters.SHOW_ACTIVE).toBeDefined();
   });
 });
+
+// describe('REDUCER', () => {
+//   it('should be exported correctly', () => {
+//     expect(reducer).toBeDefined();
+//   });
+// });
---
+++ src/reducers/reducer.js
@@ -0,0 +1,12 @@
+// import VisibilityFilters from '../actions/actions';
+
+// const initialState = {
+//   visibilityFilter: VisibilityFilters.SHOW_ALL,
+//   todos: [],
+// };
+
+// const reducer = (state = initialState, action) => {
+//   return initialState;
+// };
+
+// export default reducer;
