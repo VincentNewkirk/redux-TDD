7c9422d154d79239561b09285b7058c1e2b898bf
Vincent <vincentnewkirk@gmail.com>
2016-12-07 00:36:07 -1000

    wrote ACTIONS tests and firs succesfull ACTION CREATOR test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { VisibilityFilters } from './actions/actions';
+import * as actions from './actions/actions';
 import reducer from './reducers/reducer';
 
 it('renders without crashing', () => {
@@ -11,16 +11,46 @@ it('renders without crashing', () => {
 
 describe('ACTIONS', () => {
   it('should be exported correctly', () => {
-    expect(VisibilityFilters).toBeDefined();
+    expect(actions).toBeDefined();
   });
-  it('should have a SHOW_ALL action', () => {
-    expect(VisibilityFilters.SHOW_ALL).toBeDefined();
+  it('should have an ADD_TODO action', () => {
+    expect(actions.ADD_TODO).toBeDefined();
   });
-  it('should have a SHOW_COMPLETED action', () => {
-    expect(VisibilityFilters.SHOW_COMPLETED).toBeDefined();
+  it('should have a TOGGLE_TODO action', () => {
+    expect(actions.TOGGLE_TODO).toBeDefined();
   });
-  it('should have a SHOW_ACTIVE action', () => {
-    expect(VisibilityFilters.SHOW_ACTIVE).toBeDefined();
+  it('should have a SET_VISIBILITY_FILTER action', () => {
+    expect(actions.SET_VISIBILITY_FILTER).toBeDefined();
+  });
+  describe('VISIBILITY FILTERS', () => {
+    it('should have a SHOW_ALL key/', () => {
+      expect(actions.VisibilityFilters.SHOW_ALL).toBeDefined();
+    });
+    it('should have a SHOW_COMPLETED key/', () => {
+      expect(actions.VisibilityFilters.SHOW_COMPLETED).toBeDefined();
+    });
+    it('should have a SHOW_ACTIVE key/', () => {
+      expect(actions.VisibilityFilters.SHOW_ACTIVE).toBeDefined();
+    });
+  });
+  describe('ACTION CREATORS', () => {
+    it('addToDo action creator should exist', () => {
+      expect(actions.addToDo).toBeDefined();
+    });
+    it('addTodo should create an action to add a To Do', () => {
+      const text = 'Finish Tests';
+      const expected = {
+        type: actions.ADD_TODO,
+        text,
+      };
+      expect(actions.addToDo(text)).toEqual(expected);
+    });
+    it('toggleToDo action creator should exist', () => {
+      expect(actions.toggleToDo).toBeDefined();
+    });
+    it('setVisibilityFilter action creator should exist', () => {
+      expect(actions.setVisibilityFilter).toBeDefined();
+    });
   });
 });
 
