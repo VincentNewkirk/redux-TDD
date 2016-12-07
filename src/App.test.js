0efe537264a954dcf1249b5cf316e35aac8f004b
Vincent <vincentnewkirk@gmail.com>
2016-12-06 18:10:30 -1000

    wrote first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as actions from './actions/actions';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+describe('ACTIONS', () => {
+  it('should be defined and exist', () => {
+    expect(actions).toBeDefined();
+  });
 });
