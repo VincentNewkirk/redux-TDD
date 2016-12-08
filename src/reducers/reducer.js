dcb3aaf5271f31fab6d343341580f38558391659
Vincent <vincentnewkirk@gmail.com>
2016-12-07 21:03:08 -1000

    refactored reducer with combine reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,3 +1,4 @@
+import { combineReducers } from 'redux';
 import { VisibilityFilters, SET_VISIBILITY_FILTER, ADD_TODO, TOGGLE_TODO } from '../actions/actions';
 
 export const initialState = {
@@ -5,38 +6,74 @@ export const initialState = {
   todos: [],
 };
 
-const reducer = (state = initialState, action) => {
+const visibilityFilter = (state = initialState.visibilityFilter, action) => {
   switch (action && action.type) {
     case SET_VISIBILITY_FILTER:
-      return {
-        visibilityFilter: action.filter,
-        todos: [...state.todos],
-      };
-    case ADD_TODO:
-      return {
-        visibilityFilter: state.visibilityFilter,
+      return action.filter;
+    default:
+      return state;
+  }
+  return state;
+};
 
-        todos: [
-          ...state.todos,
-          {
-            'text': action.text,
-            'completed': false,
-          }
-        ]
-      };
+const todos = (state = initialState.todos, action) => {
+  switch (action && action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          'text': action.text,
+          'completed': false,
+        }
+      ];
     case TOGGLE_TODO:
-      const newState = state.todos.map((todo, i) => {
-        if (i === action.index) {
+      return state.map((todo, i) => {
+        if (action.index === i) {
           todo['completed'] = !todo['completed'];
         }
         return todo;
       });
-      return {
-        visibilityFilter: state.visibilityFilter,
-        todos: newState,
-      };
   }
   return state;
 };
+
+const reducer = combineReducers({
+  visibilityFilter,
+  todos,
+});
+
+// const reducer = (state = initialState, action) => {
+//   switch (action && action.type) {
+//     case SET_VISIBILITY_FILTER:
+//       return {
+//         visibilityFilter: action.filter,
+//         todos: [...state.todos],
+//       };
+//     case ADD_TODO:
+//       return {
+//         visibilityFilter: state.visibilityFilter,
+
+//         todos: [
+//           ...state.todos,
+//           {
+//             'text': action.text,
+//             'completed': false,
+//           }
+//         ]
+//       };
+//     case TOGGLE_TODO:
+//       const newState = state.todos.map((todo, i) => {
+//         if (i === action.index) {
+//           todo['completed'] = !todo['completed'];
+//         }
+//         return todo;
+//       });
+//       return {
+//         visibilityFilter: state.visibilityFilter,
+//         todos: newState,
+//       };
+//   }
+//   return state;
+// };
 
 export default reducer;
