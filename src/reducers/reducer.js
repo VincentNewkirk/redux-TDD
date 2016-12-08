891939591e8ee4cf955bbb42d4856b8fd0322b84
Vincent <vincentnewkirk@gmail.com>
2016-12-07 22:44:05 -1000

    cleaned up some code in reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -10,8 +10,6 @@ const visibilityFilter = (state = initialState.visibilityFilter, action) => {
   switch (action && action.type) {
     case SET_VISIBILITY_FILTER:
       return action.filter;
-    default:
-      return state;
   }
   return state;
 };
@@ -41,39 +39,5 @@ const reducer = combineReducers({
   visibilityFilter,
   todos,
 });
-
-// const reducer = (state = initialState, action) => {
-//   switch (action && action.type) {
-//     case SET_VISIBILITY_FILTER:
-//       return {
-//         visibilityFilter: action.filter,
-//         todos: [...state.todos],
-//       };
-//     case ADD_TODO:
-//       return {
-//         visibilityFilter: state.visibilityFilter,
-
-//         todos: [
-//           ...state.todos,
-//           {
-//             'text': action.text,
-//             'completed': false,
-//           }
-//         ]
-//       };
-//     case TOGGLE_TODO:
-//       const newState = state.todos.map((todo, i) => {
-//         if (i === action.index) {
-//           todo['completed'] = !todo['completed'];
-//         }
-//         return todo;
-//       });
-//       return {
-//         visibilityFilter: state.visibilityFilter,
-//         todos: newState,
-//       };
-//   }
-//   return state;
-// };
 
 export default reducer;
