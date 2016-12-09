6cb4682c541f4a1f4d13eebb2cb2b99484d763a5
Vincent <vincentnewkirk@gmail.com>
2016-12-08 18:48:19 -1000

    addToDo, toggleToDo and visibilityFilter test written and passed

--- src/store/store.test.js
+++ src/store/store.test.js
@@ -1,19 +1,34 @@
 import { store } from './store';
 import * as actions from '../actions/actions';
-import { VisibilityFilters } from '../actions/actions';
-
 
 describe('STORE', () => {
   it('should be exported correctly', () => {
     expect(store).toBeDefined();
   });
-  it('addToDo action creator should work', () => {
+  it('addToDo, toggleToDo and setVisibilityFilter should still work', () => {
+    store.dispatch(actions.addToDo( 'Learn about actions'));
+    store.dispatch(actions.addToDo('Learn about reducers'));
+    store.dispatch(actions.addToDo('Learn about store'));
+    store.dispatch(actions.toggleToDo(0));
+    store.dispatch(actions.toggleToDo(1));
+    store.dispatch(actions.setVisibilityFilter(actions.VisibilityFilters.SHOW_COMPLETED));
     const expected = {
-      visibilityFilter: VisibilityFilters.SHOW_ALL,
-      todos: [{ 'text': 'test', 'completed': false }],
+      todos: [
+        {
+          'completed': true,
+          'text': 'Learn about actions'
+        },
+        {
+          'completed': true,
+          'text': 'Learn about reducers'
+        },
+        {
+          'completed': false,
+          'text': 'Learn about store'
+        }
+      ],
+      visibilityFilter: 'SHOW_COMPLETED'
     };
-    const text = 'test';
-    store.dispatch(actions.addToDo(text));
     expect(store.getState()).toEqual(expected);
   });
 });
