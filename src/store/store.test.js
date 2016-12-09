a6460ab2f5e95ab1cea12125b99fe8d0005bcb73
Vincent <vincentnewkirk@gmail.com>
2016-12-08 18:34:55 -1000

    wrote test to make sure addToDo is working

--- src/store/store.test.js
+++ src/store/store.test.js
@@ -1,7 +1,20 @@
 import { store } from './store';
+import * as actions from '../actions/actions';
+import { VisibilityFilters } from '../actions/actions';
+
 
 describe('STORE', () => {
   it('should be exported correctly', () => {
     expect(store).toBeDefined();
+  });
+  it('addToDo action creator should work', () => {
+    const expected = {
+      visibilityFilter: VisibilityFilters.SHOW_ALL,
+      todos: [{ 'text': 'test', 'completed': false }],
+    };
+    const text = 'test';
+    const actual = store.dispatch(actions.addToDo(text));
+    console.log(actual);
+    expect(store.getState()).toEqual(expected);
   });
 });
