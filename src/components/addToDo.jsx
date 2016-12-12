3e40843cda0a27a932a4a630ebe3b9681cc271bc
Vincent <vincentnewkirk@gmail.com>
2016-12-12 13:47:08 -1000

    looks like add-to-do form works

--- src/components/addToDo.jsx
+++ src/components/addToDo.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class ToDoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.textChange = this.textChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.textInput = '';
+  }
+
+  textChange(event) {
+    this.textInput = event.target.value;
+    console.log(this.textInput);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.addToDo(this.textInput);
+  }
+
   render() {
     return (
       <form>
-        <input type="text" name="to-do" />
-        <input type="submit" value="Add To-Do" onClick={this.props.addToDo} />
+        <input type="text" name="to-do" onChange={this.textChange} />
+        <input type="submit" value="Add To-Do" onClick={this.handleSubmit} />
       </form>
     )
   }
